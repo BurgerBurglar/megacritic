6a53237ce86dfbb72d4f52e36953b0412990566d
Shuo Tian <tianshuo1996@outlook.com>
2021-12-29 03:06:19 -0600

    recommendation button

--- components/Recommendations.tsx
+++ components/Recommendations.tsx
@@ -1,4 +1,5 @@
 import { Flex, Heading, Image, Text } from "@chakra-ui/react";
+import NextLink from "next/link";
 import React from "react";
 import { useRecommendations } from "../hooks/useFetch";
 import { MovieCardRating } from "./MovieCardRating";
@@ -18,34 +19,39 @@ export const Recommendations: React.FC<RecommendationsProps> = ({ id }) => {
       </Heading>
       <Slider h="230px" mt={3}>
         {recommendations.slice(0, 9).map((recommendation) => (
-          <Flex
+          <NextLink
             key={recommendation.id}
-            w="300px"
-            minW="300px"
-            h="full"
-            display="inline-block"
-            m={2}
-            borderRadius="lg"
-            overflow="hidden"
+            href={"http://localhost:3000/movie/" + recommendation.id}
           >
-            <Image
-              src={
-                "https://www.themoviedb.org/t/p/w250_and_h141_face" +
-                recommendation.backdrop_path
-              }
-              alt={recommendation.title}
-              w="100%"
-            />
-            <Flex justify="space-between" p={3} whiteSpace="normal">
-              <Flex>
-                <TextElipse>{recommendation.title}</TextElipse>
-                <Text color="gray.500" ml={1}>
-                  ({recommendation.release_date.slice(0, 4)})
-                </Text>
+            <Flex
+              as="button"
+              w="300px"
+              minW="300px"
+              h="full"
+              display="inline-block"
+              m={2}
+              borderRadius="lg"
+              overflow="hidden"
+            >
+              <Image
+                src={
+                  "https://www.themoviedb.org/t/p/w250_and_h141_face" +
+                  recommendation.backdrop_path
+                }
+                alt={recommendation.title}
+                w="100%"
+              />
+              <Flex justify="space-between" p={3} whiteSpace="normal">
+                <Flex>
+                  <TextElipse>{recommendation.title}</TextElipse>
+                  <Text color="gray.500" ml={1}>
+                    ({recommendation.release_date.slice(0, 4)})
+                  </Text>
+                </Flex>
+                <MovieCardRating rating={recommendation.vote_average} />
               </Flex>
-              <MovieCardRating rating={recommendation.vote_average} />
             </Flex>
-          </Flex>
+          </NextLink>
         ))}
       </Slider>
     </Flex>
