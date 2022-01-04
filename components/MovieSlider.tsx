dc5a88aed26b9427dee16a104a15ca4cae22b283
Shuo Tian <tianshuo1996@outlook.com>
2022-01-04 17:31:50 -0600

    css for slider

--- components/MovieSlider.tsx
+++ components/MovieSlider.tsx
@@ -3,6 +3,7 @@ import React, { useRef } from "react";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick-theme.css";
 import "slick-carousel/slick/slick.css";
+import { useThemeColor } from "../hooks/useColors";
 import { useContainerDimensions } from "../hooks/useContainerDimensions";
 import { MovieOverview } from "../types/Movie";
 import { MovieCard } from "./MovieCard";
@@ -28,7 +29,21 @@ export const MovieSlider: React.FC<MovieSliderProps> = ({ movies }) => {
 
   return (
     <div className="App">
-      <Box ref={sliderContainerRef}>
+      <Box
+        ref={sliderContainerRef}
+        sx={{
+          ".slick-arrow": {
+            bg: useThemeColor(800),
+            opacity: 0.1,
+            h: "full",
+            w: 50,
+            zIndex: 1,
+          },
+          ".slick-arrow:hover": { opacity: 0.5 },
+          ".slick-arrow:before": { color: "black" },
+          ".slick-dots li button:before": { color: useThemeColor(500) },
+        }}
+      >
         <Slider {...settings}>
           {movies.map((movie) => (
             <MovieCard key={movie.id} movie={movie} />
