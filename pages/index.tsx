436cadaad4932d8a60c4d6c8f3d9fea3e22b33db
Shuo Tian <tianshuo1996@outlook.com>
2022-01-04 17:39:35 -0600

    home page shade

--- pages/index.tsx
+++ pages/index.tsx
@@ -14,11 +14,12 @@ import Head from "next/head";
 import React from "react";
 import { MovieSlider } from "../components/MovieSlider";
 import { useColorSchemeContext } from "../contexts/ColorSchemeProvider";
-import { useTenShadesOfGray } from "../hooks/useColors";
+import { useTenShadesOfGray, useThemeColor } from "../hooks/useColors";
 import { useSearch } from "../hooks/useSearch";
 import { MovieOverview } from "../types/Movie";
 import { maxW } from "../utils/constants";
 import { getMovieOverviews } from "../utils/request";
+
 interface HomeProps {
   discover: MovieOverview[];
   nowPlaying: MovieOverview[];
@@ -57,23 +58,32 @@ const Home: NextPage<HomeProps> = ({
         display="flex"
         alignItems="center"
         pos="relative"
-        bgImage={`url('${bgImage}')`}
-        bgRepeat="no-repeat"
-        bgSize="cover"
         maxW={maxW}
         h={heroHeight}
       >
         <Box
           className="bg-tint"
-          bgColor="black"
+          bgImage={`url('${bgImage}')`}
+          filter="grayscale(100%)"
+          bgRepeat="no-repeat"
+          bgSize="cover"
           pos="absolute"
           w="full"
           h="full"
           top={0}
           left={0}
-          opacity={0.5}
         />
-        <Box color="white" zIndex={1} w="full" p={5}>
+        <Box
+          className="bg-tint"
+          bgColor={useThemeColor(900, 900)}
+          pos="absolute"
+          w="full"
+          h="full"
+          top={0}
+          left={0}
+          opacity={0.4}
+        />
+        <Box color="white" zIndex={0} w="full" p={5}>
           <Text fontSize={{ base: "4xl", md: "6xl" }}>Welcome.</Text>
           <Text fontSize={{ base: "md", md: "xl" }}>
             Millions of movies, TV shows and people to discover. Explore now.
@@ -88,6 +98,7 @@ const Home: NextPage<HomeProps> = ({
               variant="solid"
               borderRadius="full"
               placeholder="star wars y:1977"
+              opacity={0.7}
               pl={5}
               value={newQuery}
               onChange={(e) => setNewQuery(e.target.value)}
